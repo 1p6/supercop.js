56ef277625fcdbfd953b63dc5a6f37b9cbbf431a
1p6 <aaapf12@gmail.com>
2016-04-16 00:15:30 -0500

    Fixed bug with browserify. See issue #1.

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 var glob = require('glob')
 var exec = require('child_process').exec
+var fs = require('fs')
 
 var files = glob.sync('vendor/src/*.c')
 var command = 'emcc supercop.c ' + files.join(' ') + ' -o lib.js -O1'
@@ -10,3 +11,12 @@ var child = exec(command, function(err){
 })
 child.stdout.pipe(process.stdout)
 child.stderr.pipe(process.stderr)
+child.on('exit', function(code){
+  if(code){
+    process.exit(code)
+  }
+  fs.appendFileSync(
+    'lib.js',
+    'if (typeof module !== "undefined") {  module["exports"] = Module; }'
+  )
+})
